e4035ae7160f62f11a418e11343821becffca14d
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-03 21:00:13 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Hello from './Hello';
 import './App.css';
-import Wrapper from './Wrapper';
+import InputSample from './InputSample';
+
 
 function App() {
  
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true} />
-      <Hello color="pink" />
-    </Wrapper>
+    <InputSample />
   )
 }
 
