fb7c1b9e55fea5619a6850fc46cfb1839da3a7c4
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-04 15:41:07 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{ useRef, useState } from 'react';
 import './App.css';
+
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
-function User({ user,onRemove }) {
-    const { username, email, id } = user;
+function User({ user,onRemove, key, email }) {
     return (
       <div>
-        <b>{username}</b> <span>({email})</span>
-        <button onClick={() => onRemove(id)}>삭제</button>
+          {console.log(user)}
+        <b>{user}</b> <span>({email})</span>
+        <button onClick={() => onRemove(key)}>삭제</button>
       </div>
     );
   }
 function UserList({ users, onRemove }) {
+
     return (
         <div>
+            {console.log(onRemove)}
            {
                users.map(
                    (user, index) => (
                         <User 
-                            user={user} 
+                            user={user.username} 
                             key={user.id} 
+                            email = {user.email}
                             onRemove={onRemove}
                         />
                    )
