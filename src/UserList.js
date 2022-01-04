494c8af24a6a7ca8e035560ccab8432bb547bffb
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-04 17:11:24 +0900

    src

--- src/UserList.js
+++ src/UserList.js
@@ -5,7 +5,14 @@ function User({ user, onRemove, onToggle }) {
     const { username, email, id, active} = user;
     useEffect(() => {
         console.log('컴포넌트가 화면에 나타남');
+        // props -> state
+        //REST API
+        // D3 Video.js
+        // setInterval, setTimeout
         return () => {
+            // clearInterval, clearTimeout
+            //라이브러리 인스터스 제거
+            //클리너함수는 뒷정리 함수라고 하면 될듯 ?
             console.log('컴포넌트가 화면에서 사라짐');
         }
     }, []);
