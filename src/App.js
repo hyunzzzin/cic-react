9fbdd5117c127c50c8a1cfb6c3a71743cc4c1f82
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-04 11:50:55 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React,{ useRef } from 'react';
 import './App.css';
 import UserList from './UserList';
 
 
 
 function App() {
- 
+    const users = [
+      {
+          id:1,
+          username: 'anhyunjin',
+          email: 'a01020813765@gmail.com'
+      },
+      {
+          id:2,
+          username: 'hohohoho',
+          email: 'hohohoho@gmail.com'
+      },
+      {
+          id:3,
+          username: 'asdslf',
+          email: 'dfsgdsg@gmail.com'
+      }
+  ];
+
+
+  const nextId = useRef(4);
+
+  const onCreate = () =>{
+    console.log(nextId.current);
+    nextId.current += 1;
+  }
+
+
   return (
-    <UserList />
+    <UserList users={users}/>
   );
 }
 
