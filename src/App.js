d01c1a6654f1c3abc7893dc28e93a6d825733c8e
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-04 18:28:43 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React,{ useRef, useState } from 'react';
+import React,{ useRef, useState, useMemo, useCallback } from 'react';
 import './App.css';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
   }
 
 function App() {
@@ -16,13 +17,13 @@ function App() {
 
   const { username, email } = inputs;
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const{ name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  };
+  }, [inputs]);
   const [users, setUsers] = useState([
       {
           id:1,
@@ -47,7 +48,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -61,22 +62,22 @@ function App() {
     });
     
     nextId.current += 1;
-  };
+  }, [username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id))
-  };
+  }, [users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
       ? {...user,active: !user.active}
       : user
     ));
-  };
+  }, [users]);
 
 
-
+  const count = useMemo(() => countActiveUsers(users),[users]);
 
   return (
     <>
@@ -87,6 +88,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수: {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -4,9 +4,9 @@ import React, { useEffect } from 'react';
 function User({ user, onRemove, onToggle }) {
     const { username, email, id, active} = user;
     
-    useEffect(() => {
-        console.log(user);
-    });
+    // useEffect(() => {
+    //     console.log(user);
+    // });
 
     return (
       <div>
