5b19159f4fbfac5411971d39971898f37a693fca
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-04 10:56:21 +0900

    src

--- src/InputSample.js
+++ src/InputSample.js
@@ -7,18 +7,40 @@ function InputSample(){
         nickname : '',
     });
 
-    const {nmae, nickname} = inputs;
+    const {name, nickname} = inputs;
     const onChange = (e) =>{
-    
+        const { name, value } = e.target;
+
+        const nextInputs = {
+            ...inputs,
+            [name]: value,
+        };
+
+        
+
+        setInputs(nextInputs);
     };
 
     const onReset = () => {
-
+        setInputs({
+            name:'',
+            nickname:'',
+        })
     };
     return (
         <div>
-            <input placeholder="이름" />
-            <input placeholder="닉네임" />
+            <input 
+                name="name" 
+                placeholder="이름"
+                onChange={onChange}
+                value={name} 
+            />
+            <input 
+                name="nickname" 
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname} 
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값 : </b>
