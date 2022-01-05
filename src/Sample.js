2463019b11f387e79248cbc14837014f5c7c8dfd
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-05 19:10:11 +0900

    src

---
+++ src/Sample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ${TM_FILNAME_BASE}() {
+    return (
+        <div>
+            Hello react!
+        </div>
+    );
+}
+
+export default ${TM_FILNAME_BASE};
