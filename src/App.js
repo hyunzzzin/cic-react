9acc215075a6747cd84b1002bb3f77de1e1e23f6
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-05 16:00:56 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,134 +1,73 @@
-import React,{ useRef, useMemo, useReducer,  useCallback } from 'react';
-import './App.css';
-import CreateUser from './CreateUser';
+import React, { useReducer, useMemo } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
-  }
+}
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: '',
-  },
   users: [
     {
-      id:1,
-      username: 'anhyunjin',
-      email: 'a01020813765@gmail.com',
-      active: true,
-  },
-  {
-      id:2,
-      username: 'hohohoho',
-      email: 'hohohoho@gmail.com',
-      active:false,
-  },
-  {
-      id:3,
-      username: 'asdslf',
-      email: 'dfsgdsg@gmail.com',
-      active:false,
-  },
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
   ]
-}
+};
 
 function reducer(state, action) {
-  switch (action.type){
-    case 'CHANGE_INPUT':
+  switch (action.type) {
+    case 'CREATE_USER':
+      return {
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
       return {
         ...state,
-        inputs:{
-          ...state.inputs,
-          [action.name]:action.value
-        }
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
       };
-      case 'CREATE_USER':
-        return {
-          inputs: initialState.inputs,
-          users: state.users.concat(action.user)
-        };
-      case 'TOGGLE_USER':
-        return {
-          ...state,
-          users: state.users.map(users =>
-            users.id === action.id
-            ? { ...users, active: !users.active }
-            : users
-            )
-        };
-      case 'REMOVE_USER':
-        return {
-          ...state,
-          users: state.users.filter(user => user.id !== action.id)
-        }
-
-      default:
-        throw new Error ('Unhandled action');
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
   }
 }
+
+// UserDispatch 라는 이름으로 내보내줍니다.
+export const UserDispatch = React.createContext(null);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
+
   const { users } = state;
-  const { username, email } = state.inputs;
 
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    });
-  },[]);
-
-
-  const onCreate = useCallback( () =>{
-    dispatch({
-      type:'CREATE_USER',
-      user: {
-        id:nextId.current,
-        username,
-        email,
-      }
-    });
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onToggle = useCallback(id => {
-    dispatch ({
-      type: 'TOGGLE_USER',
-      id
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch ({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
-
-  const count =useMemo(() => countActiveUsers(users), [users]);
-
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <>
-      <CreateUser 
-        username={username} 
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList 
-        users={users} 
-        onToggle={onToggle}
-        onRemove={onRemove}
-      />
-      <div>활성 사용자 수: {count}</div>
-    </>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
   );
 }
 
