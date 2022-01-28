0b0e142a2a6fdeec7c8dedf2b3229133f4bbf944
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-28 14:16:26 +0900

    src

--- src/ContextSample.js
+++ src/ContextSample.js
@@ -4,7 +4,7 @@ const MyContext = createContext('defaultValue');
 function Child() {
     const text = useContext(MyContext);
     return <div>안녕하세요
-        fsfds? {text}</div>
+        fsdasdasfds? {text}</div>
 }
 
 function Parent({ text }) {
